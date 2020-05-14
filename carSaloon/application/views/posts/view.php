6b144dbc1377cfab052c58e4a8ccd50557dfeb76
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2020-05-14 15:41:14 +0200

    adatbázis modulok kidolgozva
    post részletes kidolgozása
    create modul kidolgozva
    delete modul kidolgozva
    edit modul elkezdve
    weblap css-el való díszítése

--- carSaloon/application/views/posts/view.php
+++ carSaloon/application/views/posts/view.php
@@ -4,3 +4,13 @@
 	<?php echo $post['body']; ?>
 
 </div>
+
+<hr>
+<a class="btn btn-default pull-left"href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+	<input type="submit" value="Delete" class="btn btn-danger">
+
+</form>
+
+
+
