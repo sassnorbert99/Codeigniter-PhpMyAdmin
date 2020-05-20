1f8d2790099b6e5bc0f0ec06d15927d5f2f1f421
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2020-05-20 13:13:34 +0200

    update users database, homepage create
    Szerkesztve lett a users adatbázis:
    -admin mező hozzáadva(0,1 értékkel rendelkezhet)
    -homepage szerkesztve lett, mely tartalmazza a readme.md kiterjesztésű fájlt

--- carSaloon/application/views/pages/home.php
+++ carSaloon/application/views/pages/home.php
@@ -1,4 +1,22 @@
-<h2><?= $title ?></h2>
-<p>Welcome to the Luxury Car Saloon Blog Application</p>
+<h1><?= $title ?></h1>
+<h2>Welcome to the Luxury Car Saloon Blog Application</h2>
+<p>
+	Adatbázis: phpMyAdmin
+<br>
+LuxuryCar szalon építése code igniter segítségével CarSaloon mappa tartalmazza a webprogramozáshoz elkészített beadandó dolgozatomat.
+<br>
+Weblapom luxus autók kezelését teszi lehetővé egy cég számára, melyben feltudja tüntetni egyes luxus cikkjük nevét, képét tulajdonágait, valamint kommentelni lehet hozzájuk.
+<br>
+Alkalmazásomban felhasználók tudnak regisztrálni, majd a regisztrációt követően autókat feltölteni, illetve kategóriákat szerkeszteni. A felhasználó a saját posztját szerkesztheti, illetve törölheti, mások posztjait azonban csak megtekintheti és kommentelheti. Minden mezei felhasználó csak a tulajdonában lévő autókat tudja szerkeszteni illetve törölni.
+<br>
+Szerepkörök: -mezei felhasználó: regisztrálás során bekerül az adatbázisba, melynek alapból 0-ás admin szintet állít be a rendszer (0: mezei felhasználó, 1: szuperadmin) -szuperadmin: adatbázisban lehet egy felhasználó admin mezőjét átállítani 1-re, ami jelzi, hogy az adott felhasználó egyben admin is. Az admin szerepköre: Bárkinek a feltöltött posztját tudja szerkeszteni, illetve törölni azokat.
+<br>
+Importálási lehetőségek: -szoftveremben lehet importálni adatokat, melyek a postnál megjelenő import gomb nyomása után látható. -importálni csak vesszővel elválasztott csv kiterjesztésű fájlokat lehet
+<br>
+<br>
+Készítette:<br>Sass-Gyarmati Norbert<br>KBROMP
+
+
+</p>
 
 
--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost:8889
--- Létrehozás ideje: 2020. Máj 17. 16:07
+-- Létrehozás ideje: 2020. Máj 20. 11:10
 -- Kiszolgáló verziója: 5.7.26
 -- PHP verzió: 7.4.2
 
@@ -33,16 +33,18 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- A tábla adatainak kiíratása `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `zipcode`, `email`, `username`, `password`, `register_date`) VALUES
-(1, '3413', 'Sass-Gyarmati Norbert', 'sassnorbert99@gmail.com', 'sassnorbert99', 'a8f5f167f44f4964e6c998dee827110c', '2020-05-15 07:54:52'),
-(2, '3413', 'test1', 'test1@gmail.com', 'test1', 'a8f5f167f44f4964e6c998dee827110c', '2020-05-17 13:51:42');
+INSERT INTO `users` (`id`, `name`, `zipcode`, `email`, `username`, `password`, `register_date`, `admin`) VALUES
+(1, '3413', 'Sass-Gyarmati Norbert', 'sassnorbert99@gmail.com', 'sassnorbert99', 'a8f5f167f44f4964e6c998dee827110c', '2020-05-15 07:54:52', 1),
+(2, '3413', 'test1', 'test1@gmail.com', 'test1', 'a8f5f167f44f4964e6c998dee827110c', '2020-05-17 13:51:42', 0),
+(3, '3413', 'Test2', 'test2@gmai.com', 'test2', 'a8f5f167f44f4964e6c998dee827110c', '2020-05-20 10:46:26', 0);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -62,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
